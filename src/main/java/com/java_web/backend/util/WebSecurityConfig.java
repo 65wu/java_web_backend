e6c2d1dba927d0ac7b8600dc7d19e2040a2de34c
65wu <904795395@qq.com>
2020-12-21 11:51:22 +0800

    :heavy_plus_sign: 添加security，配置basic auth用户密码并关闭csrf验证

---
+++ src/main/java/com/java_web/backend/util/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.java_web.backend.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().csrf().disable();
+    }
+}
