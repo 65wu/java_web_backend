cb29531f3caa2be8de032ab6b60518df968b8d9d
65wu <904795395@qq.com>
2020-12-22 01:55:42 +0800

    :building_construction: 调整response策略

--- src/main/java/com/java_web/backend/util/MyResponse.java
+++ src/main/java/com/java_web/backend/util/MyResponse.java
@@ -1,32 +1,20 @@
 package com.java_web.backend.util;
 
 public class MyResponse {
-    private int code;
-    private String message;
-    private Object data;
+    private final int code;
+    private final String message;
+    private final Object data;
 
-    public static MyResponse success() {
-        MyResponse myResponse = new MyResponse();
-        myResponse.setResultCode(ResultCode.SUCCESS);
-        return myResponse;
+    public MyResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+        this.data = null;
     }
 
-    public static MyResponse success(Object data) {
-        MyResponse myResponse = new MyResponse();
-        myResponse.setResultCode(ResultCode.SUCCESS);
-        myResponse.setData(data);
-        return myResponse;
-    }
-
-    public static MyResponse fail() {
-        MyResponse MyResponse = new MyResponse();
-        MyResponse.setResultCode(ResultCode.FAIL);
-        return MyResponse;
-    }
-
-    private void setResultCode(ResultCode resultCode){
-        this.code = resultCode.getCode();
-        this.message = resultCode.getMessage();
+    public MyResponse(int code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
     }
 
     public int getCode() {
@@ -39,9 +27,5 @@ public class MyResponse {
 
     public Object getData() {
         return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
     }
 }
