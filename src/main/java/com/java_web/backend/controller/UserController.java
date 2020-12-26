81bf38a9ee155990398d82aafba9dc916f550677
65wu <904795395@qq.com>
2020-12-27 01:28:31 +0800

    Revert ":sparkles: 编写注册消息类"
    This reverts commit 9757888a

--- src/main/java/com/java_web/backend/controller/UserController.java
+++ src/main/java/com/java_web/backend/controller/UserController.java
@@ -4,50 +4,20 @@ import com.java_web.backend.model.User;
 import com.java_web.backend.service.UserService;
 import com.java_web.backend.util.MyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotNull;
-import java.util.Map;
+import javax.validation.Valid;
 
 @RestController
-@Validated
 public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public MyResponse Register(@RequestBody Map<String, Object> payload) {
-        class RegisterEvent {
-            @NotNull(message = "password required!")
-            private final String password;
-            @NotNull(message = "name required!")
-            private final String name;
-            @NotNull(message = "email required!")
-            private final String email;
-            RegisterEvent(String password, String name, String email) {
-                this.password = password;
-                this.name = name;
-                this.email = email;
-            }
-            public String getPassword() {
-                return password;
-            }
-            public String getName() {
-                return name;
-            }
-            public String getEmail() {
-                return email;
-            }
-        }
-        RegisterEvent registerEvent = new RegisterEvent(
-                (String)payload.get("password"),
-                (String)payload.get("name"),
-                (String)payload.get("email")
-        );
+    public MyResponse Register(@RequestBody User newUser) {
         return userService.Register(
-                registerEvent.getPassword(),
-                registerEvent.getName(),
-                registerEvent.getEmail()
+                newUser.getPassword(),
+                newUser.getName(),
+                newUser.getEmail()
         );
     }
     @PostMapping("/login")
