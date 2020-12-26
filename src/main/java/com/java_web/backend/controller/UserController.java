6734c70b751b3a6da3241be2969a940e03115792
65wu <904795395@qq.com>
2020-12-27 00:27:43 +0800

    :recycle: 改用inner class与map检测json key是否存在

--- src/main/java/com/java_web/backend/controller/UserController.java
+++ src/main/java/com/java_web/backend/controller/UserController.java
@@ -1,24 +1,53 @@
 package com.java_web.backend.controller;
 
 import com.java_web.backend.model.User;
-import com.java_web.backend.model.UserRegisterEvent;
 import com.java_web.backend.service.UserService;
 import com.java_web.backend.util.MyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
 
 @RestController
+@Validated
 public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public MyResponse Register(@RequestBody @Valid UserRegisterEvent user) {
+    public MyResponse Register(@RequestBody Map<String, Object> payload) {
+        class RegisterEvent {
+            @NotNull(message = "password required!")
+            private final String password;
+            @NotNull(message = "name required!")
+            private final String name;
+            @NotNull(message = "email required!")
+            private final String email;
+            RegisterEvent(String password, String name, String email) {
+                this.password = password;
+                this.name = name;
+                this.email = email;
+            }
+            public String getPassword() {
+                return password;
+            }
+            public String getName() {
+                return name;
+            }
+            public String getEmail() {
+                return email;
+            }
+        }
+        RegisterEvent registerEvent = new RegisterEvent(
+                (String)payload.get("password"),
+                (String)payload.get("name"),
+                (String)payload.get("email")
+        );
         return userService.Register(
-                user.getPassword(),
-                user.getName(),
-                user.getEmail()
+                registerEvent.getPassword(),
+                registerEvent.getName(),
+                registerEvent.getEmail()
         );
     }
     @PostMapping("/login")
