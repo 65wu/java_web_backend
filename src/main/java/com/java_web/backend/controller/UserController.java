f17c4eb41393b273be7b983b669c73e60515395b
65wu <904795395@qq.com>
2021-01-06 20:55:12 +0800

    :truck: 加个user mapping

--- src/main/java/com/java_web/backend/controller/UserController.java
+++ src/main/java/com/java_web/backend/controller/UserController.java
@@ -11,9 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.http.HttpHeaders;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
