668273a5fd19dfc9b6c774c2f17494203d6f49c9
65wu <904795395@qq.com>
2021-01-07 14:53:49 +0800

    :bug: 由于;导致page helper的limit未生效

--- src/main/java/com/java_web/backend/dao/News/NewsManager.java
+++ src/main/java/com/java_web/backend/dao/News/NewsManager.java
@@ -1,7 +1,6 @@
 package com.java_web.backend.dao.News;
 
 import com.java_web.backend.model.dto.News.NewsBasic;
-import com.java_web.backend.model.po.News;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,7 +13,7 @@ public interface NewsManager {
                    type,
                    publish_time
             FROM news
-            INNER JOIN news_type USING(type_id);
+            INNER JOIN news_type USING(type_id)
             """)
     ArrayList<NewsBasic> getNewsAll();
 }
