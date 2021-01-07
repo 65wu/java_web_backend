fc33fcf1000055d378306a42470bee5280c93eea
65wu <904795395@qq.com>
2021-01-07 14:23:36 +0800

    :sparkles: 因为jpa返回敏感字段过多，自定义get all

--- src/main/java/com/java_web/backend/dao/News/NewsManager.java
+++ src/main/java/com/java_web/backend/dao/News/NewsManager.java
@@ -1,9 +1,19 @@
 package com.java_web.backend.dao.News;
 
+import com.java_web.backend.model.po.News;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.ArrayList;
+
 @Mapper
 public interface NewsManager {
-
+    @Select("""
+            SELECT content,
+                   type,
+                   publish_time
+            FROM news
+            INNER JOIN news_type USING(type_id);
+            """)
+    ArrayList<News> getNewsAll();
 }
--- src/main/java/com/java_web/backend/service/NewsService.java
+++ src/main/java/com/java_web/backend/service/NewsService.java
@@ -1,5 +1,6 @@
 package com.java_web.backend.service;
 
+import com.java_web.backend.dao.News.NewsManager;
 import com.java_web.backend.dao.News.NewsRepository;
 import com.java_web.backend.model.po.News;
 import com.java_web.backend.util.MyResponse;
@@ -12,8 +13,10 @@ import java.util.ArrayList;
 public class NewsService {
     @Autowired
     private NewsRepository newsRepository;
+    @Autowired
+    private NewsManager newsManager;
     public MyResponse GetAll() {
-        ArrayList<News> newsList = (ArrayList<News>) newsRepository.findAll();
+        ArrayList<News> newsList = newsManager.getNewsAll();
         return new MyResponse(
                 1,
                 "获取成功",
