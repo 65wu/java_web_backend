b42c5c7e6286da94be3eddb9fabcaed04e346d84
65wu <904795395@qq.com>
2021-01-07 17:00:12 +0800

    :sparkles: 编写基本邮件发送类

---
+++ src/main/java/com/java_web/backend/util/EmailSender.java
@@ -0,0 +1,26 @@
+package com.java_web.backend.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class EmailSender {
+    @Resource
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
