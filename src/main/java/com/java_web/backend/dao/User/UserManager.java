a4795cf95c68629b867518731ea8341640689bf7
65wu <904795395@qq.com>
2021-01-08 01:08:35 +0800

    :art: sql大小写规范

--- src/main/java/com/java_web/backend/dao/User/UserManager.java
+++ src/main/java/com/java_web/backend/dao/User/UserManager.java
@@ -4,18 +4,18 @@ import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserManager {
-    @Update("update user set password=#{password} where user_id=#{userId}")
+    @Update("UPDATE user SET password=#{password} WHERE user_id=#{userId}")
     void updateUserPassword(
             @Param("password") String password,
             @Param("userId") Integer userId
     );
-    @Update("update user set email=#{email}, nickname=#{nickname} where user_id=#{userId}")
+    @Update("UPDATE user SET email=#{email}, nickname=#{nickname} WHERE user_id=#{userId}")
     void updateUserBasic(
             @Param("email") String email,
             @Param("nickname") String nickname,
             @Param("userId") Integer userId
     );
-    @Select("select user_id from user where username=#{username}")
+    @Select("SELECT user_id FROM user WHERE username=#{username}")
     Integer findIdByUsername(
             @Param("username") String username
     );
